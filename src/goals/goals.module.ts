df7bf5ccdec85dffa0726f74f3d6895219900446
awaismehr <awaismehr001@gmail.com>
2023-06-14 21:16:57 +0500

    solve goal entity error

--- src/goals/goals.module.ts
+++ src/goals/goals.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GoalsService } from './goals.service';
 import { GoalsController } from './goals.controller';
-import { Goal } from './entities/goal.entity';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GoalSchema } from './goal.schema';
+import { Goal, GoalSchema } from './goal.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Goal.name, schema: GoalSchema }])],
