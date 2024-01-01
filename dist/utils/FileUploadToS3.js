7c6d6ac3ef655ce9bc79ac429905feff08700a3d
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2024-01-01 22:08:02 +0500

    updated

--- dist/utils/FileUploadToS3.js
+++ dist/utils/FileUploadToS3.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const multerS3 = require("multer-s3-transform");
-const sharp = require("sharp");
 const AWS = require("aws-sdk");
 require('dotenv').config();
 class FileUploadToS3 {
@@ -12,27 +11,12 @@ class FileUploadToS3 {
                 secretAccessKey: process.env.AWS_SECRET_KEY,
             }),
             bucket: process.env.AWS_BUCKET_NAME,
-            transforms: [
-                {
-                    id: 'original',
-                    key: function (req, file, cb) {
-                        cb(null, `${file.originalname}`);
-                    },
-                    transform: function (req, file, cb) {
-                        cb(null, sharp().png());
-                    }
-                },
-                {
-                    id: 'large',
-                    key: (req, file, cb) => cb(null, new Date().getTime() + `_large_${file.originalname}`),
-                    transform: (req, file, cb) => cb(null, sharp().resize(1200, 900).png())
-                },
-                {
-                    id: 'small',
-                    key: (req, file, cb) => cb(null, new Date().getTime() + `_small_${file.originalname}`),
-                    transform: (req, file, cb) => cb(null, sharp().resize(400, 300).png())
-                }
-            ]
+            metadata: function (req, file, cb) {
+                cb(null, { fieldName: file.originalname });
+            },
+            key: function (req, file, cb) {
+                cb(null, Date.now().toString() + "_" + file.originalname);
+            }
         });
     }
 }
